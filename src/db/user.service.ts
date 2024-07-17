d298d75ac0e242ae8511dd2ea4d277f7016e0bd7
kldvp <prasadkodali786@gmail.com>
2024-07-17 19:34:37 +0530

    Add user & auth service

--- src/db/user.service.ts
+++ src/db/user.service.ts
@@ -6,7 +6,9 @@ import { User, UserDocument } from '../schemas/user.schema';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>
+  ) {}
 
   // creating new user record
   async createUser(email: string, name: string, password: string): Promise<User> {
