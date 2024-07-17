597636c191d469abc064fb7a10e9a83eab367d2a
kldvp <prasadkodali786@gmail.com>
2024-07-17 19:13:48 +0530

    Add user schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URL)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
